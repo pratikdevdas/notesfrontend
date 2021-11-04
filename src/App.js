0f4161dd17b739a812f7b0f63ab3c0cde6b95eac
Rev <dasdev.pratik@gmail.com>
2021-11-04 07:24:05 +0530

    part 5a (handling login)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
   const [errorMessage, setErrorMessage] = useState(null)
-
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  
   useEffect(() => {
     noteService
       .getAll()
@@ -62,10 +66,64 @@ const App = () => {
   ? notes
   : notes.filter(note => note.important)
 
-  return (
+  const handleLogin = async(event) =>{
+    event.preventDefault()
+    console.log('logging with',username, password)
+
+    try {
+      const user = await loginService.login({
+        username,password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }catch(exception){
+      setErrorMessage('Wrong credentials')
+      setTimeout(()=>{
+        setErrorMessage(null)
+      },5000)
+    }
+    }
+
+    //we dont use curly bracket beacuse it returns only one form element i.e its the shortform arrow function
+ const loginForm = () => (
+ <form onSubmit={handleLogin}>
+  <div>username
+    <input type="text" value={username} name="Username" onChange={({target})=>setUsername(target.value)}/>
+  </div>
+  <div>
+    password
+  <input type="password" value={password} name="Password" onChange={({target})=>{console.log(target.value)
+  return setPassword(target.value)}}/>
+  </div>
+  <button type="submit">login</button>
+</form>
+) 
+// implemented without refactoring in noteForm function i.e return keyword not removed
+const noteForm = () => {return (<form onSubmit={addNote}>
+  <input
+    value={newNote}
+    onChange={handleNoteChange}
+  />
+  <button type="submit">save</button>
+</form>  )}
+return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      
+      {/* {user === null && loginForm} */}
+      {/* {user !== null && noteForm} */}
+{/* conditional literal of above code. If user === null is truthy, loginForm() is executed. If not, noteForm() is.*/}
+     {user === null ?
+     loginForm() :
+     <div>
+      <p>{user.name} logged-in</p>
+     {noteForm()}
+     </div>
+    //  If the user is logged-in, their name is shown on the screen functionality above  
+     }
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -80,13 +138,7 @@ const App = () => {
             />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>  
+      
       <Footer />
     </div>
   )
