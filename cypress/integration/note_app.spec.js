3ce0682eba2c95dfe7e2976f00a9ac413063bc3c
revenant <dasdev.pratik@gmail.com>
2021-11-27 06:34:25 +0530

    failed login test implemented

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -9,10 +9,25 @@ describe('Note app', function() {
     cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
-
+  // it.only run specific test
   it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2021')
+  })
+
+  it.only('login fails with wrong password', function(){
+    cy.contains('log in').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+    // Using should is a bit trickier than using contains, but it allows for more diverse tests than contains which works based on text content only.
+    cy.get('.error').should('contain','Wrong credentials')
+    // make sure that the error message is red and it has a border:
+    // Because all tests are for the same component we accessed using cy.get, we can chain them using and.
+      .and('have.css','color', 'rgb(255, 0, 0)')
+      .and('have.css','border-style','solid')
+    cy.get('html').should('not.contain', 'Matti')
+
   })
 
   it('login form can be opened', function() {
